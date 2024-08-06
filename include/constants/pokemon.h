e012d94c880e46f92822f2b8f434896191d16e53
Typher0 <136129884+Typher0@users.noreply.github.com>
2024-08-05 21:46:09 -0700

    Update pokemon.h

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -22,9 +22,8 @@
 #define TYPE_DRAGON           17
 #define TYPE_DARK             18
 #define TYPE_FAIRY            19
-#define TYPE_SOUND            20
-#define TYPE_STELLAR          21
-#define NUMBER_OF_MON_TYPES   22
+#define TYPE_STELLAR          20
+#define NUMBER_OF_MON_TYPES   21
 
 // Pokémon egg groups
 #define EGG_GROUP_NONE                0
