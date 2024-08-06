5a276f54f2b84f7e5d3feade29586942966bebb8
Typher0 <136129884+Typher0@users.noreply.github.com>
2024-08-05 20:59:53 -0700

    Update gen_8_families.h

--- src/data/pokemon/species_info/gen_8_families.h
+++ src/data/pokemon/species_info/gen_8_families.h
@@ -3097,7 +3097,15 @@ const struct SpeciesInfo gSpeciesInfoGen8[] =
         .iconSprite = gMonIcon_ToxtricityGigantamax,
         .iconPalIndex = 0,
         FOOTPRINT(Toxtricity)
-        .isMegaEvolution = TRUE,
+        OVERWORLD(
+            sPicTable_ToxtricityLowKey,
+            SIZE_32x32,
+            SHADOW_SIZE_M,
+            TRACKS_FOOT,
+            gOverworldPalette_ToxtricityLowKey,
+            gShinyOverworldPalette_ToxtricityLowKey
+        )
+        .isGigantamax = TRUE,
         .levelUpLearnset = sToxtricityLowKeyLevelUpLearnset,
         .teachableLearnset = sToxtricityLowKeyTeachableLearnset,
         .formSpeciesIdTable = sToxtricityFormSpeciesIdTable,
