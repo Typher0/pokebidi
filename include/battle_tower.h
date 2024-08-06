89af1fe741f61d71f1f972e14d9cb4a994700577
Typher0 <136129884+Typher0@users.noreply.github.com>
2024-08-05 19:15:16 -0700

    Update battle_tower.h

--- include/battle_tower.h
+++ include/battle_tower.h
@@ -36,9 +36,8 @@ struct FacilityMon
 extern const u8 gTowerMaleFacilityClasses[30];
 extern const u16 gTowerMaleTrainerGfxIds[30];
 extern const u8 gTowerFemaleFacilityClasses[20];
-extern const u16 gTowerFemaleTrainerGfxIds[20];
-extern const u16 gBattleFrontierHeldItems[];
-extern const struct FacilityMon gBattleFrontierMons[];
+extern const u8 gTowerFemaleTrainerGfxIds[20];
+extern const struct TrainerMon gBattleFrontierMons[];
 extern const struct BattleFrontierTrainer gBattleFrontierTrainers[];
 extern const struct FacilityMon gSlateportBattleTentMons[];
 extern const struct BattleFrontierTrainer gSlateportBattleTentTrainers[];
