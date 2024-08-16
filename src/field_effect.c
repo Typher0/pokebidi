a1400271cf3db765a6e11b5434a0c51012523c95
IvyAnonymous <weavile2000@gmail.com>
2024-08-16 16:26:58 -0700

    Revert "Update field_effect.c"
    This reverts commit 3b827581bcc2226848bb00372d66401eec41a60e.

--- src/field_effect.c
+++ src/field_effect.c
@@ -3139,8 +3139,8 @@ u8 FldEff_RayquazaSpotlight(void)
 
 u8 FldEff_NPCFlyOut(void)
 {
-    struct Sprite *sprite;
         u8 spriteId;
+    struct Sprite *sprite;
 
     sprite->oam.paletteNum = LoadPlayerObjectEventPalette(gSaveBlock2Ptr->playerGender);
     sprite->oam.priority = 1;
