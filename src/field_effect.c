e4bfac1aee2be93b1bcd3f7bd3ab19751d483ce8
IvyAnonymous <62076017+IvyAnonymous@users.noreply.github.com>
2024-08-16 16:31:17 -0700

    Update field_effect.c

--- src/field_effect.c
+++ src/field_effect.c
@@ -3139,8 +3139,8 @@ u8 FldEff_RayquazaSpotlight(void)
 
 u8 FldEff_NPCFlyOut(void)
 {
-        u8 spriteId;
-    struct Sprite *sprite;
+        u8 spriteId = Sprite;
+    struct Sprite *sprite = spriteId;
 
     sprite->oam.paletteNum = LoadPlayerObjectEventPalette(gSaveBlock2Ptr->playerGender);
     sprite->oam.priority = 1;
