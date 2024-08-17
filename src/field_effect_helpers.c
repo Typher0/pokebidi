ba6db0f08a18cffac11b346ef747c315dabf9fbb
IvyAnonymous <62076017+IvyAnonymous@users.noreply.github.com>
2024-08-16 17:06:11 -0700

    Update field_effect_helpers.c

--- src/field_effect_helpers.c
+++ src/field_effect_helpers.c
@@ -1522,7 +1522,7 @@ static u32 ShowDisguiseFieldEffect(u8 fldEff, u8 templateIdx)
     if (spriteId != MAX_SPRITES)
     {
         struct Sprite *sprite = &gSprites[spriteId];
-        UpdateSpritePaletteByTemplate(gFieldEffectObjectTemplatePointers[FLDEFFOBJ], sprite);
+        UpdateSpritePaletteByTemplate(gFieldEffectObjectTemplatePointers[FldEff], sprite);
         sprite->coordOffsetEnabled ++;
         sprite->sFldEff = fldEff;
         sprite->sLocalId = gFieldEffectArguments[0];
