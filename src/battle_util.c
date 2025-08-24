d4d4476d18e15873ec9fb4729723a17d40de8563
Typher0 <136129884+Typher0@users.noreply.github.com>
2025-08-23 21:57:49 -0700

    Update battle_util.c

--- src/battle_util.c
+++ src/battle_util.c
@@ -4653,7 +4653,7 @@ u32 AbilityBattleEffects(u32 caseID, u32 battler, u32 ability, u32 special, u32 
         {
             u32 abilityAtk = GetBattlerAbility(gBattlerAttacker);
             if ((!IS_BATTLER_OF_TYPE(gBattlerAttacker, TYPE_GRASS) || B_POWDER_GRASS < GEN_6)
-             && abilityAtk != ABILITY_OVERCOAT
+             && abilityAtk != ABILITY_OVERCOAT && ABILITY_COZY
              && GetBattlerHoldEffect(gBattlerAttacker, TRUE) != HOLD_EFFECT_SAFETY_GOGGLES)
             {
                 u32 poison, paralysis, sleep;
