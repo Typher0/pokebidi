1e93551716f3d52039c4c2b392a34282ba94f641
Typher0 <136129884+Typher0@users.noreply.github.com>
2026-02-04 18:43:52 -0800

    Region Map Layout

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1158,3 +1158,9 @@ EventScript_VsSeekerChargingDone::
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
 	.include "data/scripts/dexnav.inc"
+
+	.include "data/maps/KingstonCity/scripts.inc"
+
+	.include "data/maps/GanariaTown/scripts.inc"
+
+	.include "data/maps/OcchitaVillage/scripts.inc"
