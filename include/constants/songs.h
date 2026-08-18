ab0566a8d2c206f3ae4ee5726918964badc18f90
IvyAnonymous <62076017+IvyAnonymous@users.noreply.github.com>
2026-08-18 02:49:02 -0500

    music updates
    rival fight and arkane city

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -577,7 +577,10 @@
 #define MUS_REDBURRO_CITY           642
 #define MUS_CABRILLO_CITY           643
 #define MUS_ROUTE_648               644
+#define MUS_VS_BANJIME_RIVAL        645
 #define MUS_DESERT                  645 // MUS_ASHROAD -- NOTE: not yet present in song_table.inc, must be added there before this ID will play anything
+#define MUS_VS_SINGULARITY_ADMIN    646
+#define MUS_ARKANE_CITY             647
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
--- src/debug.c
+++ src/debug.c
@@ -4348,7 +4348,10 @@ static void DebugAction_DestroyFollowerNPC(u8 taskId)
     X(MUS_ZEPHYR_TOWN) \
     X(MUS_REDBURRO_CITY) \
     X(MUS_CABRILLO_CITY) \
-    X(MUS_ROUTE_648)
+    X(MUS_ROUTE_648) \
+    X(MUS_VS_BANJIME_RIVAL) \
+    X(MUS_VS_SINGULARITY_ADMIN) \
+    X(MUS_ARKANE_CITY)
 
 #define SOUND_LIST_SE               \
     X(SE_USE_ITEM)                  \
