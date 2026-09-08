a27fbf3ac51c85231cf149ff8b3959f1d98da59a
Typher0 <136129884+Typher0@users.noreply.github.com>
2026-09-07 18:58:17 -0700

    Revert "Merge branch 'rh-hideout:master' into master"
    This reverts commit 490c81e603ee3aafbf74adddf8c2390dae1ad7da, reversing
    changes made to 16aa0472915da0147571fe825bb8fb1a6229e819.

--- src/battle_ai_main.c
+++ src/battle_ai_main.c
@@ -5314,7 +5314,6 @@ static s32 AI_CalcMoveEffectScore(enum BattlerId battlerAtk, enum BattlerId batt
         break;
     }
     case EFFECT_TERRAIN:
-    {
         enum BattleTerrain terrain = GetMoveTerrainType(move);
 
         if (ShouldSetTerrain(battlerAtk, terrain))
@@ -5328,7 +5327,6 @@ static s32 AI_CalcMoveEffectScore(enum BattlerId battlerAtk, enum BattlerId batt
                 ADJUST_SCORE(WEAK_EFFECT);
         }
         break;
-    }
     case EFFECT_STEEL_ROLLER:
         {
             u32 terrain = gFieldTimers.terrain;
